99763dda526d0438db61e17dfa606083eec2c8ec
Paul Sun <paulsun096@gmail.com>
2020-01-31 14:10:24 -0500

    Full App v1

---
+++ Launcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics; //interact w/ system processes and event logs
+
+namespace GameLauncher
+{
+    class Launcher
+    {
+        public void PlayGame()
+        {
+            Process.Start("C:/Users/Mike/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Steam/cuphead.url");
+        }
+        
+        public void LaunchWebsite(string url)
+        {
+            Process.Start(url);
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GameLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void LaunchGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            //fix, only use 1 instance of var l
+            var l = new Launcher();
+            l.PlayGame();
+
+        }
+    }
+}
